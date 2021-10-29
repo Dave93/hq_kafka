2e7c8d491928089e4df66fb5ad16d5120bca95b4
Davr <yul.davron.93@gmail.com>
2021-10-29 10:18:35 +0500

    changes to rabbit

---
+++ actions/new_basket_line_added.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+const fs = require('fs');
+const path = require('path')
+require('dotenv').config()
+
+const action = async ($data) => {
+    console.log('action data', $data);
+
+    try {
+        await axios.post(`${process.env.B24_API_URL}newline.basket.lead`, {
+            data: $data
+        });
+    } catch (e) {
+
+    }
+}
+
+module.exports = action;
--- index.js
+++ index.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const fs = require("fs");
 const path = require("path");
-const { Kafka } = require("kafkajs");
+// const { Kafka } = require("kafkajs");
 const { Markup, Scenes, Telegraf } = require("telegraf");
 const telegrafPlugin = require("fastify-telegraf");
 const fastify = require("fastify");
+const amqp = require('amqplib/callback_api');
+
+const { AMQP_VHOST, AMQP_HOST, AMQP_PORT, AMQP_USER, AMQP_PASSWORD } = process.env;
+
+const AMQP_ENDPOINT = `amqp://${AMQP_USER}:${AMQP_PASSWORD}@${AMQP_HOST}:${AMQP_PORT}${AMQP_VHOST}`;
 
 
 // Connection URL
@@ -53,120 +58,254 @@ function formatNumber(num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");
 }
 
-(async () => {
-  const kafka = new Kafka({
-    // clientId: "my-app",
-    brokers: [process.env.KAFKA_BORKER],
-    // sasl: {
-    //   mechanism: "scram-sha-512", // scram-sha-256 or scram-sha-512
-    //   // username: "chopar_davr", // producer
-    //   // password: "mNw7@T8P`theQj#T", // producer
-    //   username: "hq_consumer", // consumer
-    //   password: 'C_22!%tt77YS*A&y', // consumer
-    // },
-    // ssl: {
-    //   ca: [fs.readFileSync(path.join(__dirname + "/ca.crt"), "utf-8")],
-    // },
-  });
-  const consumer = kafka.consumer({ groupId: "hq" });
+// (async () => {
+//   const kafka = new Kafka({
+//     // clientId: "my-app",
+//     brokers: [process.env.KAFKA_BORKER],
+//     // sasl: {
+//     //   mechanism: "scram-sha-512", // scram-sha-256 or scram-sha-512
+//     //   // username: "chopar_davr", // producer
+//     //   // password: "mNw7@T8P`theQj#T", // producer
+//     //   username: "hq_consumer", // consumer
+//     //   password: 'C_22!%tt77YS*A&y', // consumer
+//     // },
+//     // ssl: {
+//     //   ca: [fs.readFileSync(path.join(__dirname + "/ca.crt"), "utf-8")],
+//     // },
+//   });
+//   const consumer = kafka.consumer({ groupId: "hq" });
+//
+//   await consumer.connect();
+//   await consumer.subscribe({ topic: "hq"/*, fromBeginning: true*/ });
+//
+//   await consumer.run({
+//     eachMessage: async ({ topic, partition, message }) => {
+//       // console.log(topic)
+//       // console.log({
+//       //   value: message.value.toString(),
+//       // });
+//
+//       // console.log(JSON.parse(message.value.toString()))
+//         console.log(message)
+//       try {
+//           const mess = JSON.parse(message.value.toString());
+//           // console.log(mess.action);
+//           // console.log('fileExists', fs.existsSync(path.join(`${__dirname}/actions/${mess.action}.js`)))
+//
+//           if (mess.action == 'order_to_tg_group') {
+//               fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(mess.data));
+//               let order = mess.data;
+//               let paymentType = order.type;
+//               if (paymentType.toLowerCase() == 'offline') {
+//                   paymentType = 'Наличными';
+//               }
+//               let deliverType = (order.delivery_type == 'deliver' ? '🚘 Доставка' : '🚶 самовывоз');
+//               let basketItems = '';
+//               let totalPrice = 0;
+//               order.basket.lines.map(line => {
+//                   let lineTotalPrice = +line.total;
+//                   totalPrice += lineTotalPrice * line.quantity;
+//                   if (line.modifiers) {
+//                       line.modifiers.map((mod) => {
+//                           lineTotalPrice -= mod.price;
+//                       });
+//                   }
+//                   basketItems += `<b>${line?.variant?.product?.attribute_data?.name?.chopar?.ru}</b>
+// ${line.quantity} x ${formatNumber(
+//                       +lineTotalPrice
+//                   )} = ${formatNumber(+lineTotalPrice * line.quantity)} сум`;
+//
+//                   if (line.modifiers) {
+//                       line.modifiers.map((mod) => {
+//                           basketItems += `\n-- ${line.quantity} x ${
+//                               mod.name
+//                           } = ${formatNumber(mod.price * line.quantity)} сум`;
+//                       });
+//                   }
+//                   basketItems += "\n\n";
+//               });
+//
+//               basketItems += '----------------------------\n';
+//               basketItems += `<b>ИТОГО: ${formatNumber(totalPrice)} сум</b>`
+//               let message = `<b>Номер заказа:</b> #${order.id}
+//
+// <b>Система:</b> Веб-сайт
+// <b>Клиент:</b> ${order.user.name}
+// <b>Телефон:</b> ${order.shipping_phone}
+// <b>Адрес:</b> ${order.shipping_address}${(order.house ? ', дом ' + order.house : '')}${(order.flat ? ', квартира ' + order.flat : '')}${(order.entrance ? ', подъезд ' + order.entrance : '')}${(order.door_code ? ', код от домофона ' + order.door_code : '')}
+// <b>Способ оплаты:</b> ${paymentType}
+// <b>Способ доставки:</b> ${deliverType}
+// <b>Филиал:</b> ${order.terminalData.name}
+// <b>Состав заказа:</b>
+//
+// ${basketItems}
+// `
+//
+//               await bot.telegram.sendMessage(order.terminalData.tg_group, message, {
+//                   parse_mode: 'HTML'
+//               });
+//               if (order.delivery_type == 'deliver') {
+//                   await  bot.telegram.sendLocation(order.terminalData.tg_group, order.lat, order.lon);
+//               }
+//               await bot.telegram.sendMessage(-758028372, message, {
+//                   parse_mode: 'HTML'
+//               });
+//               if (order.delivery_type == 'deliver') {
+//                   await  bot.telegram.sendLocation(-758028372, order.lat, order.lon);
+//               }
+//
+//               return;
+//           }
+//           // console.log('fileExists', fs.existsSync(path.join(`${__dirname}/actions/${mess.action}.js`)))
+//           // const action = require(path.join(`${__dirname}/actions/${mess.action}.js`));
+//           // await action(mess.data)
+//
+//           if (mess.action == 'send_order_later') {
+//               // Use connect method to connect to the server
+//               await client.connect();
+//               const db = client.db(dbName);
+//               const collection = db.collection("documents");
+//               await collection.insertOne({
+//                   ...mess.data,
+//                   sent: false
+//               });
+//               client.close();
+//               return;
+//           }
+//         const action = require(path.join(`${__dirname}/actions/${mess.action}.js`));
+//         await action(mess.data)
+//       } catch (e) {
+//         console.log(e);
+//       }
+//
+//     },
+//   });
+// })();
 
-  await consumer.connect();
-  await consumer.subscribe({ topic: "hq"/*, fromBeginning: true*/ });
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      // console.log(topic)
-      // console.log({
-      //   value: message.value.toString(),
-      // });
+amqp.connect(AMQP_ENDPOINT, function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var exchange = 'actions';
 
-      // console.log(JSON.parse(message.value.toString()))
-        console.log(message)
-      try {
-          const mess = JSON.parse(message.value.toString());
-          // console.log(mess.action);
-          // console.log('fileExists', fs.existsSync(path.join(`${__dirname}/actions/${mess.action}.js`)))
+        channel.assertExchange(exchange, 'fanout', {
+            durable: false
+        });
 
-          if (mess.action == 'order_to_tg_group') {
-              fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(mess.data));
-              let order = mess.data;
-              let paymentType = order.type;
-              if (paymentType.toLowerCase() == 'offline') {
-                  paymentType = 'Наличными';
-              }
-              let deliverType = (order.delivery_type == 'deliver' ? '🚘 Доставка' : '🚶 самовывоз');
-              let basketItems = '';
-              let totalPrice = 0;
-              order.basket.lines.map(line => {
-                  let lineTotalPrice = +line.total;
-                  totalPrice += lineTotalPrice * line.quantity;
-                  if (line.modifiers) {
-                      line.modifiers.map((mod) => {
-                          lineTotalPrice -= mod.price;
-                      });
-                  }
-                  basketItems += `<b>${line?.variant?.product?.attribute_data?.name?.chopar?.ru}</b>
-${line.quantity} x ${formatNumber(
-                      +lineTotalPrice
-                  )} = ${formatNumber(+lineTotalPrice * line.quantity)} сум`;
+        channel.assertQueue('', {
+            exclusive: true
+        }, function(error2, q) {
+            if (error2) {
+                throw error2;
+            }
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+            channel.bindQueue(q.queue, exchange, '');
 
-                  if (line.modifiers) {
-                      line.modifiers.map((mod) => {
-                          basketItems += `\n-- ${line.quantity} x ${
-                              mod.name
-                          } = ${formatNumber(mod.price * line.quantity)} сум`;
-                      });
-                  }
-                  basketItems += "\n\n";
-              });
+            channel.consume(q.queue, async mess => {
+                if (mess.content) {
+                    try {
+                        mess = JSON.parse(mess.content.toString());
+                        console.log(mess);
 
-              basketItems += '----------------------------\n';
-              basketItems += `<b>ИТОГО: ${formatNumber(totalPrice)} сум</b>`
-              let message = `<b>Номер заказа:</b> #${order.id}
+                          if (mess.action == 'order_to_tg_group') {
+                              fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(mess.data));
+                              let order = mess.data;
+                              let paymentType = order.type;
+                              if (paymentType.toLowerCase() == 'offline') {
+                                  paymentType = 'Наличными';
+                              }
+                              let deliverType = (order.delivery_type == 'deliver' ? '🚘 Доставка' : '🚶 самовывоз');
+                              let basketItems = '';
+                              let totalPrice = 0;
+                              order.basket.lines.map(line => {
+                                  let lineTotalPrice = +line.total;
+                                  totalPrice += lineTotalPrice * line.quantity;
+                                  if (line.modifiers) {
+                                      line.modifiers.map((mod) => {
+                                          lineTotalPrice -= mod.price;
+                                      });
+                                  }
+                                  basketItems += `<b>${line?.variant?.product?.attribute_data?.name?.chopar?.ru}</b>
+    ${line.quantity} x ${formatNumber(
+                                      +lineTotalPrice
+                                  )} = ${formatNumber(+lineTotalPrice * line.quantity)} сум`;
 
-<b>Система:</b> Веб-сайт
-<b>Клиент:</b> ${order.user.name}
-<b>Телефон:</b> ${order.shipping_phone}
-<b>Адрес:</b> ${order.shipping_address}${(order.house ? ', дом ' + order.house : '')}${(order.flat ? ', квартира ' + order.flat : '')}${(order.entrance ? ', подъезд ' + order.entrance : '')}${(order.door_code ? ', код от домофона ' + order.door_code : '')}
-<b>Способ оплаты:</b> ${paymentType}
-<b>Способ доставки:</b> ${deliverType}
-<b>Филиал:</b> ${order.terminalData.name}
-<b>Состав заказа:</b>
+                                  if (line.modifiers) {
+                                      line.modifiers.map((mod) => {
+                                          basketItems += `\n-- ${line.quantity} x ${
+                                              mod.name
+                                          } = ${formatNumber(mod.price * line.quantity)} сум`;
+                                      });
+                                  }
+                                  basketItems += "\n\n";
+                              });
 
-${basketItems}
-`
+                              basketItems += '----------------------------\n';
+                              basketItems += `<b>ИТОГО: ${formatNumber(totalPrice)} сум</b>`
+                              let message = `<b>Номер заказа:</b> #${order.id}
 
-              await bot.telegram.sendMessage(order.terminalData.tg_group, message, {
-                  parse_mode: 'HTML'
-              });
-              if (order.delivery_type == 'deliver') {
-                  bot.telegram.sendLocation(order.terminalData.tg_group, order.lat, order.lon);
-              }
+    <b>Система:</b> Веб-сайт
+    <b>Клиент:</b> ${order.user.name}
+    <b>Телефон:</b> ${order.shipping_phone}
+    <b>Адрес:</b> ${order.shipping_address}${(order.house ? ', дом ' + order.house : '')}${(order.flat ? ', квартира ' + order.flat : '')}${(order.entrance ? ', подъезд ' + order.entrance : '')}${(order.door_code ? ', код от домофона ' + order.door_code : '')}
+    <b>Способ оплаты:</b> ${paymentType}
+    <b>Способ доставки:</b> ${deliverType}
+    <b>Филиал:</b> ${order.terminalData.name}
+    <b>Состав заказа:</b>
 
-              return;
-          }
-          // console.log('fileExists', fs.existsSync(path.join(`${__dirname}/actions/${mess.action}.js`)))
-          // const action = require(path.join(`${__dirname}/actions/${mess.action}.js`));
-          // await action(mess.data)
+    ${basketItems}
+    `
 
-          if (mess.action == 'send_order_later') {
-              // Use connect method to connect to the server
-              await client.connect();
-              const db = client.db(dbName);
-              const collection = db.collection("documents");
-              await collection.insertOne({
-                  ...mess.data,
-                  sent: false
-              });
-              client.close();
-              return;
-          }
-        const action = require(path.join(`${__dirname}/actions/${mess.action}.js`));
-        await action(mess.data)
-      } catch (e) {
-        console.log(e);
-      }
+                              await bot.telegram.sendMessage(order.terminalData.tg_group, message, {
+                                  parse_mode: 'HTML'
+                              });
+                              if (order.delivery_type == 'deliver') {
+                                  await bot.telegram.sendLocation(order.terminalData.tg_group, order.lat, order.lon);
+                              }
+                              await bot.telegram.sendMessage(-758028372, message, {
+                                  parse_mode: 'HTML'
+                              });
+                              if (order.delivery_type == 'deliver') {
+                                  await bot.telegram.sendLocation(-758028372, order.lat, order.lon);
+                              }
 
-    },
-  });
-})();
+                              return;
+                          }
+                        // console.log('fileExists', fs.existsSync(path.join(`${__dirname}/actions/${mess.action}.js`)))
+                        // const action = require(path.join(`${__dirname}/actions/${mess.action}.js`));
+                        // await action(mess.data)
+
+                        if (mess.action == 'send_order_later') {
+                            // Use connect method to connect to the server
+                            await client.connect();
+                            const db = client.db(dbName);
+                            const collection = db.collection("documents");
+                            await collection.insertOne({
+                                ...mess.data,
+                                sent: false
+                            });
+                            client.close();
+                            return;
+                        }
+
+
+                        if(mess.action) {
+                            console.log(path.join(__dirname, `/actions/${mess.action}`));
+                            const action = require(path.join(__dirname, `/actions/${mess.action}`));
+                            await action(mess.data);
+                        }
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+            }, {
+                noAck: true
+            });
+        });
+    });
+});
